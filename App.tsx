65c59ef13e6cb1bd70f623982b005fdce025bb10
Le Van Thuan My <51257500+levanthuanmy@users.noreply.github.com>
2022-02-23 22:57:42 +0700

    handle login screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import { useState } from "react"
+import {
+  Button,
+  Dimensions,
+  Image,
+  StyleSheet,
+  TextInput,
+  View,
+} from "react-native"
+import welcomeBanner from "./assets/welcome.png"
 
 export default function App() {
+  const [signInData, setSignInData] = useState({ username: "", password: "" })
+  console.log("App - signInData", signInData)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Image source={welcomeBanner} style={styles.image}></Image>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(username) =>
+          setSignInData({ username, password: signInData.password })
+        }
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={(password) =>
+          setSignInData({ username: signInData.username, password })
+        }
+      />
+
+      <Button onPress={() => {}} title="Sign in" />
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
-});
+  image: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height / 2.5,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#000",
+    paddingHorizontal: 8,
+  },
+})
---
+++ src/screens/sign-in/index.tsx
@@ -0,0 +1,8 @@
+import React from "react"
+import { View } from "react-native"
+
+const SignInScreen = () => {
+  return <View>SignInScreen</View>
+}
+
+export default SignInScreen
