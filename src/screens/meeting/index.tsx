9584f4f74ff7f59f75997b554f413f296638d511
Le Van Thuan My <51257500+levanthuanmy@users.noreply.github.com>
2022-03-03 20:21:10 +0700

    update meeting screen

--- src/screens/meeting/index.tsx
+++ src/screens/meeting/index.tsx
@@ -1,14 +1,41 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import React, { FC } from "react"
-import { View } from "react-native"
+import { Dimensions, SafeAreaView, StatusBar, StyleSheet, View } from "react-native"
 import { RootStackParamList } from "../../../App"
 
 type Props = NativeStackScreenProps<RootStackParamList, "Meeting">
 
 const MeetingScreen: FC<Props> = ({ navigation }) => {
-  return <View>
-
-  </View>
+  return (
+    <SafeAreaView style={meetingStyles.container}>
+      <View style={meetingStyles.statusBar}></View>
+      <View style={meetingStyles.actionBar}></View>
+    </SafeAreaView>
+  )
 }
+
+const meetingStyles = StyleSheet.create({
+  container: {
+    height: Dimensions.get("window").height - 64,
+    paddingTop: StatusBar.currentHeight,
+    backgroundColor: "#000011",
+    position: "relative",
+  },
+  actionBar: {
+    // width: Dimensions.get("window").width - 30,
+    height: 80,
+    backgroundColor: "#fff",
+    position: "absolute",
+    bottom: 20,
+    width: "100%",
+  },
+  statusBar:{
+    height: 80,
+    backgroundColor: "#fff",
+    position: "absolute",
+    top: 20,
+    width: "100%",
+  }
+})
 
 export default MeetingScreen
